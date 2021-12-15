28b38580f5a90410d29b937dc82d64f71c4c805d
Ray's Git <lancelot53.ra@gmail.com>
2021-12-15 17:05:00 -0500

    Completed

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
+  #age_array = []
+  lowest_value = nil
+  low_key = nil
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      low_key = k
+      lowest_value = v
+      #age_array << age
+    end
+  end
+  #binding.pry
+  low_key
 end
+
+#lowest = key_for_min_value(name_hash)
+#puts name_hash.each_pair {|name,age| puts name if age == lowest }
